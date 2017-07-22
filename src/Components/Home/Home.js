a77d6382dcad51958e09dee2fe8f68f6c3868dbe
IuliiaKot <kotlenko.julia@gmail.com>
2017-07-22 16:56:49 -0400

    add About and Home component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+class Home extends Component {
+  render(){
+    return(
+      <div className="jumbotron">
+        <h1>Pl Administration</h1>
+        <p> React, Redux and React Router</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    )
+  }
+}
+
+
+export default Home;
