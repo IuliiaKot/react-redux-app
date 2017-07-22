33f9d0c9e183e92c3001939fc12b1bcc58b39463
IuliiaKot <kotlenko.julia@gmail.com>
2017-07-22 16:59:52 -0400

    Add App component

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+
+class App extends Component {
+  render(){
+    return(
+      <div className="container-fluid">
+      <p>Header here ...</p>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
